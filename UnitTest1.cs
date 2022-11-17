573092d808083c45227dca15a7956222d340e150
520H0557 <114124289+520H0557@users.noreply.github.com>
2022-11-17 10:39:26 +0700

    Ex3_NoTestCases

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestConsole;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
